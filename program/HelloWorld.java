24500bd5b904c3a298e889453592dabe09a41dcd
Jegadeeswari S <78527874+jegadeeswari554@users.noreply.github.com>
2022-07-28 15:59:14 +0530

    Add files via upload

---
+++ program/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+@Author: Jegadeeswari S
+Date : 28/07/2022
+Title : HelloWorld Program
+Question : Executing Basic Java Program
+*/
+class HelloWorld
+{
+	// Calling main method
+	public static void main(String[]args)
+	{
+		System.out.println(30+30+"then"+30+50+60+90);
+		System.out.println(30+30+"then"+(30+50+60+90));
+		System.out.println(30+30+"then"+(30+50)+(60+90));
+		System.out.println(30+30+"then"+((30+50)+(60+90)));
+
+	}
+}
+
